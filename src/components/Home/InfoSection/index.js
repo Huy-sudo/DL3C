5b84fdb3881cab008f27fb0c87ad94ae54314edb
Ubuntu <azureuser@custom-k8s.cluster.local>
2022-12-10 09:21:59 +0000

    working on Homepage

--- src/components/Home/InfoSection/index.js
+++ src/components/Home/InfoSection/index.js
@@ -5,22 +5,22 @@ import styles from "./index.module.css"
 function InfoSection() {
     const [info, setInfo] = useState([
         {
-            id: "info1",
+            id: "1",
             title: "<h4>Lối kiến trúc <i>flamboyant</i> của thời Đệ tam Cộng hòa Pháp</h4>",
             description: "<p>Nhà hát Thành phố Hồ Chí Minh - với lịch sử phát triển hàng trăm năm - được xem như cái nôi \"nghệ thuật\" tại địa phương, nơi có các chương trình biểu diễn quy mô lớn"
         },
         {
-            id: "info2",
+            id: "2",
             title: "<h4>Lối kiến trúc <i>flamboyant</i> của thời Đệ tam Cộng hòa Pháp</h4>",
             description: "<p>Nhà hát Thành phố Hồ Chí Minh - với lịch sử phát triển hàng trăm năm - được xem như cái nôi \"nghệ thuật\" tại địa phương, nơi có các chương trình biểu diễn quy mô lớn</p>"
         },
         {
-            id: "info3",
+            id: "3",
             title: "<h4>Lối kiến trúc <i>flamboyant</i> của thời Đệ tam Cộng hòa Pháp</h4>",
             description: "<p>Nhà hát Thành phố Hồ Chí Minh - với lịch sử phát triển hàng trăm năm - được xem như cái nôi \"nghệ thuật\" tại địa phương, nơi có các chương trình biểu diễn quy mô lớn</p>"
         },
         {
-            id: "info4",
+            id: "4",
             title: "<h4>Lối kiến trúc <i>flamboyant</i> của thời Đệ tam Cộng hòa Pháp</h4>",
             description: "<p>Nhà hát Thành phố Hồ Chí Minh - với lịch sử phát triển hàng trăm năm - được xem như cái nôi \"nghệ thuật\" tại địa phương, nơi có các chương trình biểu diễn quy mô lớn</p>"
         }
@@ -32,7 +32,7 @@ function InfoSection() {
             <div className={styles.item_box}>
                 {info.map(item => {
                     return (
-                        <InfoBlock key={item.id} title={item.title} description={item.description} />
+                        <InfoBlock key={item.id} title={item.title} description={item.description} id={item.id} />
                     )
                 })}
             </div>

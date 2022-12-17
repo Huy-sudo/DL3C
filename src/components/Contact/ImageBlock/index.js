7c14598a6342880b74932468569564293467cdc0
Ubuntu <nhd@nhd.dokn23wvjidezix3rfe3vw0j0c.lx.internal.cloudapp.net>
2022-12-17 02:15:30 +0000

    final

--- src/components/Contact/ImageBlock/index.js
+++ src/components/Contact/ImageBlock/index.js
@@ -1,9 +1,10 @@
 import React from 'react'
-
+import styles from "./index.module.css"
 function ImageBlock(props) {
   return (
     <div>
         <img src={props.url} alt="" width={"380px"} height="280px"></img>
+        <span><p className={styles.title}>{props.title}</p></span>
     </div>
   )
 }
